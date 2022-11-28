215909820ccc672345ac83eb73e40ef0392611f2
tekkeni <93118484+tekkeni@users.noreply.github.com>
2022-11-28 15:46:44 +0900

    Add files via upload

---
+++ train/losses.py
@@ -0,0 +1,237 @@
+"""
+Lovasz-Softmax and Jaccard hinge loss in PyTorch
+Maxim Berman 2018 ESAT-PSI KU Leuven (MIT License)
+"""
+
+from __future__ import print_function, division
+
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+try:
+    from itertools import  ifilterfalse
+except ImportError: # py3k
+    from itertools import  filterfalse as ifilterfalse
+
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1. - intersection / union
+    if p > 1: # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+
+
+# --------------------------- MULTICLASS LOSSES ---------------------------
+
+
+def CrossEntropyLoss2d_weight(args):
+
+    weight = torch.ones(args.num_classes)
+
+    if args.num_classes==18:
+        weight[0] = 2.8149201869965	
+        weight[1] = 6.9850029945374	
+        weight[2] = 3.7890393733978	
+        weight[3] = 9.9428062438965	
+        weight[4] = 9.7702074050903	
+        weight[5] = 9.5110931396484	
+        weight[6] = 10.311357498169	
+        weight[7] = 10.026463508606	
+        weight[8] = 4.6323022842407	
+        weight[9] = 9.5608062744141	
+        weight[10] = 7.8698215484619	
+        weight[11] = 9.5168733596802	
+        weight[12] = 10.373730659485	
+        weight[13] = 6.6616044044495	
+        weight[14] = 10.260489463806	
+        weight[15] = 10.287888526917	
+        weight[16] = 10.289801597595	
+    #         weight[17] = 10.405355453491	
+    #         weight[18] = 10.138095855713	
+        weight[17]= 0        
+
+    elif args.num_classes==20:
+        weight[0] = 2.8149201869965	
+        weight[1] = 6.9850029945374	
+        weight[2] = 3.7890393733978	
+        weight[3] = 9.9428062438965	
+        weight[4] = 9.7702074050903	
+        weight[5] = 9.5110931396484	
+        weight[6] = 10.311357498169	
+        weight[7] = 10.026463508606	
+        weight[8] = 4.6323022842407	
+        weight[9] = 9.5608062744141	
+        weight[10] = 7.8698215484619	
+        weight[11] = 9.5168733596802	
+        weight[12] = 10.373730659485	
+        weight[13] = 6.6616044044495	
+        weight[14] = 10.260489463806	
+        weight[15] = 10.287888526917	
+        weight[16] = 10.289801597595	
+        weight[17] = 10.405355453491	
+        weight[18] = 10.138095855713	
+        weight[19]= 0               
+
+    elif args.num_classes==8:
+        weight[0] = 2.8149201869965	
+        weight[1] = 7.8698215484619
+        weight[2] = 9.5110931396484	
+        weight[3] = 4.6323022842407	
+        weight[4] = 7.8698215484619
+        weight[5] = 9.5168733596802	
+        weight[6] = 6.6616044044495	
+        weight[7] = 0.0            
+#            weight[7] = 4.0            
+
+
+    return weight
+
+
+class CrossEntropyLoss2d(torch.nn.Module):
+    def __init__(self, weight=None):
+        super().__init__()
+        self.loss = torch.nn.NLLLoss(weight)
+    def forward(self, outputs, targets):
+        return self.loss(torch.nn.functional.log_softmax(outputs, dim=1), targets)
+
+
+def weighted_lovasz_softmax(probas, labels, only_present=False, per_image=False, ignore=None, weight=None):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+      per_image: compute the loss per image instead of per batch
+      ignore: void class labels
+    """
+    
+
+    
+    if per_image:
+        loss = mean(weighted_lovasz_softmax_flat(*flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), only_present=only_present, weight=weight)
+                          for prob, lab in zip(probas, labels))
+    else:
+        loss = weighted_lovasz_softmax_flat(*flatten_probas(probas, labels, ignore), only_present=only_present, weight=weight)
+    return loss
+
+
+def weighted_lovasz_softmax_flat(probas, labels, only_present=False, weight=None):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+    """
+    if probas.numel() == 0:
+        # only void pixels, the gradients should be 0
+        return probas * 0.
+    C = probas.size(1)
+    
+    losses = []
+    for c in range(C):
+        fg = (labels == c).float() # foreground for class c     
+        if only_present and fg.sum() == 0:
+            continue
+        errors = (Variable(fg) - probas[:, c]).abs() * weight[c]
+        errors_sorted, perm = torch.sort(errors, 0, descending=True)
+        perm = perm.data
+        fg_sorted = fg[perm]
+        losses.append(torch.dot(errors_sorted, Variable(lovasz_grad(fg_sorted))))
+    return mean(losses)
+
+    
+def lovasz_softmax(probas, labels, only_present=False, per_image=False, ignore=None):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+      per_image: compute the loss per image instead of per batch
+      ignore: void class labels
+    """
+    if per_image:
+        loss = mean(lovasz_softmax_flat(*flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), only_present=only_present)
+                          for prob, lab in zip(probas, labels))
+    else:
+        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore), only_present=only_present)
+    return loss
+
+
+
+def lovasz_softmax_flat(probas, labels, only_present=False):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+    """
+    if probas.numel() == 0:
+        # only void pixels, the gradients should be 0
+        return probas * 0.
+    C = probas.size(1)
+    
+    losses = []
+    for c in range(C):
+        fg = (labels == c).float() # foreground for class c     
+        if only_present and fg.sum() == 0:
+            continue
+        errors = (Variable(fg) - probas[:, c]).abs()
+        errors_sorted, perm = torch.sort(errors, 0, descending=True)
+        perm = perm.data
+        fg_sorted = fg[perm]
+        losses.append(torch.dot(errors_sorted, Variable(lovasz_grad(fg_sorted))))
+    return mean(losses)
+
+
+
+def flatten_probas(probas, labels, ignore=None):
+    """
+    Flattens predictions in the batch
+    """
+    B, C, H, W = probas.size()
+    probas = probas.permute(0, 2, 3, 1).contiguous().view(-1, C)  # B * H * W, C = P, C
+    labels = labels.view(-1)
+    if ignore is None:
+        return probas, labels
+    valid = (labels != ignore)
+    vprobas = probas[valid.nonzero().squeeze()]
+    vlabels = labels[valid]
+    return vprobas, vlabels
+
+
+
+
+# --------------------------- HELPER FUNCTIONS ---------------------------
+def isnan(x):
+    return x != x
+    
+    
+def mean(l, ignore_nan=True, empty=0):
+    """
+    nanmean compatible with generators.
+    """
+    l = iter(l)
+    if ignore_nan:
+        l = ifilterfalse(isnan, l)
+    try:
+        n = 1
+        acc = next(l)
+    except StopIteration:
+        if empty == 'raise':
+            raise ValueError('Empty mean')
+        return empty
+    for n, v in enumerate(l, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
