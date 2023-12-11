daa6694f074fcfcb8d5a65b54bdde71e9ef1fe42
tekkeni <93118484+tekkeni@users.noreply.github.com>
2023-12-11 15:05:28 +0900

    Update dataset.py

--- train/dataset.py
+++ train/dataset.py
@@ -171,3 +171,75 @@ class KITTI360_LiDAR(Dataset):
             
     def __len__(self):
         return len(self.filenamesGt)
+
+
+class KITTI360_2CAMs_LiDAR2(Dataset):
+    def __init__(self, transform=None, subset='train', args=None):    
+        if args == None:
+            subsamplingRate = 3
+            datadir1 = '/home/tekken/dataset/KITTI360/semantic_data'
+        else:
+            subsamplingRate = args.subsamplingRate
+            datadir1 = args.datadir1
+        
+        
+        self.dataset_root= datadir1
+        self.subset = subset
+  
+        
+        print(f"Dataset root for {subset} set: {self.dataset_root+'/semantic_labels/' + subset}")
+        if subset == 'train':
+            filenameGT_path = self.dataset_root + '/semantic_labels/*/*/semantic_trainids/*.png'
+        else:
+            filenameGT_path = self.dataset_root + '/semantic_labels/' + subset + '/*/semantic_trainids/*.png'
+        filenamesGt = glob.glob(filenameGT_path)
+        filenamesGt.sort()
+        
+        ##  학습의 효율을 위해 1/n 만 사용 ###
+        for i in range(len(filenamesGt)-1, 0, -1):
+            if i % subsamplingRate != 0:         
+                del filenamesGt[i]
+    
+        num_images = len(filenamesGt)
+        print(f'number of used {subset} images: {num_images}')           
+        
+        self.filenamesGt = filenamesGt
+        self.co_transform = transform
+        
+    def __getitem__(self, index):
+        filenameGt = self.filenamesGt[index]
+        filenameLD = filenameGt.replace('/semantic_data/semantic_labels/train/', '/data_3d_raw/')        
+        filenameLD = filenameLD.replace('/semantic_data/semantic_labels/val/', '/data_3d_raw/')        
+#         filenameLD = filenameGt.replace('/semantic_data/semantic_labels/'+ self.subset + '/', '/data_3d_raw/')        
+        filenameLD = filenameLD.replace('/semantic_trainids/', '/velodyne_points/data/')
+        filenameLD = filenameLD.replace('.png', '.bin')
+        
+        with open(filenameLD, 'rb') as f:
+            LiDAR_points = np.fromfile(f, dtype=np.float32)
+            LiDAR_points = np.reshape(LiDAR_points,[-1,4])        
+
+        filename = filenameGt.replace('/semantic_labels/', '/semantic_images/')        
+        filename2 = filename.replace('/semantic_trainids/', '/data_rect_r/')
+        filename = filename.replace('/semantic_trainids/', '/data_rect/')
+        
+        image = io.read_image(filename)
+        if image.shape[0] == 4:
+            image = image[:3]           
+      
+        image_r = io.read_image(filename2)
+        if image_r.shape[0] == 4:
+            image_r = image_r[:3]      
+
+        label = io.read_image(filenameGt)          
+            
+        if self.co_transform is not None:
+            PriorTransData = [LiDAR_points, image, image_r, label]
+            PostTransData = self.co_transform(PriorTransData)            
+
+        return PostTransData         
+            
+
+         
+        
+    def __len__(self):
+        return len(self.filenamesGt)           
