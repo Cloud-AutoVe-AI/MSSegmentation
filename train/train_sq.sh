015bf5cc654f9e1c1ed3b8f874d80769bbc4e4fb
tekkeni <93118484+tekkeni@users.noreply.github.com>
2024-12-13 15:55:53 +0900

    Update train_sq.sh

--- train/train_sq.sh
+++ train/train_sq.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=0,1,2 python main_2cams_group.py \
  --crop_height 256 \
  --num_classes 8 \
  --ignore_class -1 \
- --datadir1 '/home/tekken/dataset/KITTI360/semantic_data' \
+ --datadir1 '/home/dataset_path' \
  --resize_width 1408 \
  --init_learningrate 0.5e-3 \
  --alpha 0.25 \
